8796e842e5fcb6ec2b1fa5f7701ca2cfebea9a71
anshuman-patra <anshumanpatra845@gmail.com>
2020-08-05 18:21:37 +0530

    count of head and tail

--- flipcoinsimulator.sh
+++ flipcoinsimulator.sh
@@ -1,10 +1,18 @@
 #!/bin/bash 
-
+read -p "enter  the number of times you want to flip the coin" n
+hc=0
+tc=0
+for (( i=0; i<$n; i++))
+do
 z=$((RANDOM%2))
 if [ $z == 0 ]
 then
-echo " head"
+(( hc++ ))
+#echo " head"
 else
-echo "tails"
+(( tc ++ ))
+#echo "tails"
 fi
-
+done
+echo " no of head " $hc
+echo "no of tails "$tc 
