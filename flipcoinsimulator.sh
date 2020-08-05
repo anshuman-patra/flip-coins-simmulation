fc14d1ae3e418606280238620a4e8bb954af4bdd
anshuman-patra <anshumanpatra845@gmail.com>
2020-08-05 21:41:24 +0530

    reapeting untill difference is 2

--- flipcoinsimulator.sh
+++ flipcoinsimulator.sh
@@ -1,7 +1,9 @@
-#!/bin/bash 
+#!/bin/bash  
 read -p "enter  the number of times you want to flip the coin" n
 hc=0
 tc=0
+function flip ()
+{
 for (( i=0; i<$n; i++))
 do
 z=$((RANDOM%2))
@@ -16,24 +18,54 @@ fi
 done
 echo " no of head " $hc
 echo "no of tails "$tc 
-if [[ $n -le 43 &&  $hc -ge 21 || $tc -ge 21 ]]
-then
-if [[ $hc -gt $tc ]]
-then
-sum=$(( $hc - $tc ))
-echo " win with head by difference of "$sum
-else
-sum=$(( $tc - $hc ))
-echo " win with the tail by differnece of "$sum
-fi
-elif [[ $hc -eq $tc ]]
-then 
-echo"draw"
-elif  [[ $hc -gt $tc ]]
-then
-sum=$(( $hc - $tc ))
-echo " win with head by difference of "$sum
-else
-sum=$(( $tc - $hc ))
-fi
 
+	if [[ $n -gt 21  &&  $hc -ge 21 || $tc -ge 21 ]]
+	then
+		if [[ $hc -gt $tc ]]
+		then
+		sum=$(( $hc - $tc ))
+			if [[  $sum -ge 2 ]]
+			then
+			echo " win with head by difference of "$sum
+			else
+			( flip )
+			fi
+
+		else
+		sum=$(( $tc - $hc ))
+			if [[  $sum -ge 2 ]]
+			then
+			echo " win with tail by difference of "$sum
+			else
+			(flip )
+			fi
+
+		fi
+	
+ elif  [[ $n -lt 21 && $hc -gt $tc ]]
+                then
+                sum=$(( $hc - $tc ))
+                if [[  $sum -ge 2 ]]
+                then
+                echo " win with head by difference of "$sum
+                else
+                ( flip )
+                fi
+
+	elif  [[ $n -lt 21 && $tc -gt $hc ]]
+	then
+	sum=$(( $tc - $hc ))
+		if [[  $sum -ge 2 ]]
+		then
+		echo " win with tails by difference of "$sum	
+		else
+		( flip )
+		fi
+	
+fi
+}
+ if [[ $hc -eq $tc ]]
+        then
+        echo "draw"
+        ( flip )
+fi
